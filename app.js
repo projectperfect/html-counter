7c2c79dca099cff860eb139399f9b68f9a7e77cc
projectperfect <jprfct@protonmail.com>
2022-09-16 22:38:01 -0500

    add event listeners on buttons. event listener on btn added to decrease count

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ let count = 0;
 // select value and buttons
 const value = document.querySelector('#value');
 const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')) {
+            count--;
+        }
+        value.textContent = count;
+    });
+});
